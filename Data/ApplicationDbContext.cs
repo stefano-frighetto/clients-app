61dfb8bce207acc12f8dc5311c0add27b4099b88
frighste <frighettostefano@gmail.com>
2025-02-13 08:49:34 -0300

    Add EF Core & Create Model and Controller

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using ClientApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ClientApi.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Client> Clients { get; set; }
+    }
+}
---
+++ Models/Client.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ClientApi.Models
+{
+    public class Client
+    {
+        public int ClientId { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        public DateTime? Birthdate { get; set; }
+        [Required]
+        public string CUIT { get; set; }
+        public string? Address { get; set; }
+        [Required]
+        [Phone]
+        //[RegularExpression]
+        public string CellPhone { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+    }
+}
