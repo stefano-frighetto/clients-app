0a2ebbf38b2a425f518ce39f4ecc24a8c6c9e3de
frighste <frighettostefano@gmail.com>
2025-11-18 21:51:11 -0300

    Replace SQL Server with PostgreSQL

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(connectionString));
+    options.UseNpgsql(connectionString));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
