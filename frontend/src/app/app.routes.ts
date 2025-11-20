160b908beb1e29c0bd17b36fd109438a349c1c57
frighste <frighettostefano@gmail.com>
2025-11-20 18:01:13 -0300

    Initial component and Tweaks - Environment set

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { ClientList } from './components/client-list/client-list';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: ClientList }
+];
