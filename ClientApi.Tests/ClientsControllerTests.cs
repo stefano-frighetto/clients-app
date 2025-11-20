434d279ea6ba9e38eed9b0135d1a619cf0889ed0
frighste <frighettostefano@gmail.com>
2025-11-20 01:20:40 -0300

    Update Test Cases to follow pattern

--- ClientApi.Tests/ClientsControllerTests.cs
+++ ClientApi.Tests/ClientsControllerTests.cs
@@ -1,9 +1,8 @@
 ﻿using ClientApi.Controllers;
-using ClientApi.Data;
 using ClientApi.DTOs;
 using ClientApi.Models;
+using ClientApi.Repositories;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using Moq;
 using Xunit;
@@ -12,32 +11,32 @@ namespace ClientApi.Tests
 {
     public class ClientsControllerTests
     {
-        private ApplicationDbContext GetDatabaseContext()
-        {
-            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
-                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
-                .Options;
+        private readonly Mock<IClientRepository> _mockRepo;
+        private readonly Mock<ILogger<ClientsController>> _mockLogger;
+        private readonly ClientsController _controller;
 
-            var databaseContext = new ApplicationDbContext(options);
-            databaseContext.Database.EnsureCreated();
-            return databaseContext;
+        public ClientsControllerTests()
+        {
+            _mockRepo = new Mock<IClientRepository>();
+            _mockLogger = new Mock<ILogger<ClientsController>>();
+            _controller = new ClientsController(_mockRepo.Object, _mockLogger.Object);
         }
 
         [Fact]
         public async Task GetClients_ReturnsOkResult_WithListOfClients()
         {
             // Arrange
-            var dbContext = GetDatabaseContext();
+            var fakeClients = new List<Client>
+            {
+                new Client { ClientId = 1, FirstName = "Juan", LastName = "Perez", CorporateName = "A", CUIT = "20-11111111-1", Email = "j@t.com", CellPhone = "111", Birthdate = DateTime.Now },
+                new Client { ClientId = 2, FirstName = "Maria", LastName = "Gomez", CorporateName = "B", CUIT = "27-22222222-2", Email = "m@t.com", CellPhone = "222", Birthdate = DateTime.Now }
+            };
 
-            dbContext.Clients.Add(new Client { FirstName = "Juan", LastName = "Perez", CorporateName = "Juan Corp", CUIT = "20-11111111-1", Email = "juan@test.com", CellPhone = "1122334455", Birthdate = DateTime.Now });
-            dbContext.Clients.Add(new Client { FirstName = "Maria", LastName = "Gomez", CorporateName = "Maria SRL", CUIT = "27-22222222-2", Email = "maria@test.com", CellPhone = "1122334466", Birthdate = DateTime.Now });
-            await dbContext.SaveChangesAsync();
-
-            var mockLogger = new Mock<ILogger<ClientsController>>();
-            var controller = new ClientsController(dbContext, mockLogger.Object);
+            _mockRepo.Setup(repo => repo.GetAllAsync())
+                     .ReturnsAsync(fakeClients);
 
             // Act
-            var result = await controller.GetClients();
+            var result = await _controller.GetClients();
 
             // Assert
             var actionResult = Assert.IsType<OkObjectResult>(result.Result);
@@ -46,220 +45,234 @@ namespace ClientApi.Tests
         }
 
         [Fact]
+        public async Task GetClient_ReturnsOk_WhenClientExists()
+        {
+            // Arrange
+            var fakeClient = new Client { ClientId = 1, FirstName = "Test", LastName = "T", CorporateName = "C", CUIT = "20-11111111-1", Email = "t@t.com", CellPhone = "111", Birthdate = DateTime.Now };
+
+            _mockRepo.Setup(repo => repo.GetByIdAsync(1))
+                     .ReturnsAsync(fakeClient);
+
+            // Act
+            var result = await _controller.GetClient(1);
+
+            // Assert
+            var actionResult = Assert.IsType<OkObjectResult>(result.Result);
+            var returnedClient = Assert.IsType<Client>(actionResult.Value);
+            Assert.Equal(1, returnedClient.ClientId);
+        }
+
+        [Fact]
+        public async Task GetClient_ReturnsNotFound_WhenClientDoesNotExist()
+        {
+            // Arrange
+            _mockRepo.Setup(repo => repo.GetByIdAsync(999))
+                     .ReturnsAsync((Client?)null);
+
+            // Act
+            var result = await _controller.GetClient(999);
+
+            // Assert
+            Assert.IsType<NotFoundObjectResult>(result.Result);
+        }
+
+        [Fact]
+        public async Task SearchClients_ReturnsOk_WhenMatchesFound()
+        {
+            // Arrange
+            var searchResults = new List<Client>
+            {
+                new Client { ClientId = 1, FirstName = "Juan", LastName = "Perez", CorporateName = "A", CUIT = "20-11111111-1", Email = "j@t.com", CellPhone = "111", Birthdate = DateTime.Now }
+            };
+
+            _mockRepo.Setup(repo => repo.SearchByNameAsync("ua"))
+                     .ReturnsAsync(searchResults);
+
+            // Act
+            var result = await _controller.SearchClientsByName("ua");
+
+            // Assert
+            var actionResult = Assert.IsType<OkObjectResult>(result.Result);
+            var returnedClients = Assert.IsType<List<Client>>(actionResult.Value);
+            Assert.Single(returnedClients);
+        }
+
+        [Fact]
         public async Task CreateClient_ReturnsCreated_WhenDataIsValid()
         {
             // Arrange
-            var dbContext = GetDatabaseContext();
-            var mockLogger = new Mock<ILogger<ClientsController>>();
-            var controller = new ClientsController(dbContext, mockLogger.Object);
-
-            var newClientDto = new CreateClientDto
+            var newDto = new CreateClientDto
             {
                 FirstName = "Nuevo",
-                LastName = "Cliente",
-                CorporateName = "New SA",
+                LastName = "C",
+                CorporateName = "N",
                 CUIT = "20-33333333-3",
-                Email = "new@test.com",
-                CellPhone = "1122334477",
+                Email = "n@t.com",
+                CellPhone = "333",
                 Birthdate = DateTime.Now
             };
 
+            _mockRepo.Setup(repo => repo.GetConflictAsync(newDto.CUIT, newDto.Email))
+                     .ReturnsAsync((Client?)null);
+
             // Act
-            var result = await controller.CreateClient(newClientDto);
+            var result = await _controller.CreateClient(newDto);
 
             // Assert
-            var createdAtActionResult = Assert.IsType<CreatedAtActionResult>(result.Result);
-            var createdClient = Assert.IsType<Client>(createdAtActionResult.Value);
-            Assert.Equal("Nuevo", createdClient.FirstName);
-            Assert.NotEqual(0, createdClient.ClientId);
+            var createdResult = Assert.IsType<CreatedAtActionResult>(result.Result);
+
+            _mockRepo.Verify(repo => repo.AddAsync(It.IsAny<Client>()), Times.Once);
+        }
+
+        [Fact]
+        public async Task CreateClient_ReturnsConflict_WhenConflictExists()
+        {
+            // Arrange
+            var newDto = new CreateClientDto
+            {
+                FirstName = "Duplicado",
+                LastName = "D",
+                CorporateName = "D",
+                CUIT = "20-33333333-3",
+                Email = "dup@t.com",
+                CellPhone = "333",
+                Birthdate = DateTime.Now
+            };
+
+            var existingClient = new Client { ClientId = 5, FirstName = "Viejo", LastName = "V", CorporateName = "V", CUIT = "20-33333333-3", Email = "otro@t.com", CellPhone = "111", Birthdate = DateTime.Now };
+
+            _mockRepo.Setup(repo => repo.GetConflictAsync(newDto.CUIT, newDto.Email))
+                     .ReturnsAsync(existingClient);
+
+            // Act
+            var result = await _controller.CreateClient(newDto);
+
+            // Assert
+            Assert.IsType<ConflictObjectResult>(result.Result);
+
+            _mockRepo.Verify(repo => repo.AddAsync(It.IsAny<Client>()), Times.Never);
         }
 
         [Fact]
         public async Task CreateClient_ReturnsConflict_WhenEmailExists()
         {
             // Arrange
-            var dbContext = GetDatabaseContext();
-            dbContext.Clients.Add(new Client { FirstName = "Existente", LastName = "Uno", CorporateName = "A", CUIT = "20-99999999-9", Email = "duplicado@test.com", CellPhone = "1111111111", Birthdate = DateTime.Now });
-            await dbContext.SaveChangesAsync();
-
-            var mockLogger = new Mock<ILogger<ClientsController>>();
-            var controller = new ClientsController(dbContext, mockLogger.Object);
-
-            var dtoConEmailDuplicado = new CreateClientDto
-            {
-                FirstName = "Intruso",
-                LastName = "Dos",
-                CorporateName = "B",
-                CUIT = "20-88888888-8",
-                Email = "duplicado@test.com",
-                CellPhone = "2222222222",
-                Birthdate = DateTime.Now
-            };
-
-            // Act
-            var result = await controller.CreateClient(dtoConEmailDuplicado);
-
-            // Assert
-            var conflictResult = Assert.IsType<ConflictObjectResult>(result.Result);
-            Assert.Contains("duplicado@test.com", conflictResult.Value?.ToString());
-        }
-
-        [Fact]
-        public async Task DeleteClient_ReturnsNoContent_WhenClientExists()
-        {
-            // Arrange
-            var dbContext = GetDatabaseContext();
-            var clientToDelete = new Client { FirstName = "Borrar", LastName = "Me", CorporateName = "Bye", CUIT = "20-55555555-5", Email = "bye@test.com", CellPhone = "1155555555", Birthdate = DateTime.Now };
-            dbContext.Clients.Add(clientToDelete);
-            await dbContext.SaveChangesAsync();
-
-            var mockLogger = new Mock<ILogger<ClientsController>>();
-            var controller = new ClientsController(dbContext, mockLogger.Object);
-
-            // Act
-            var result = await controller.DeleteClient(clientToDelete.ClientId);
-
-            // Assert
-            Assert.IsType<NoContentResult>(result);
-            Assert.Null(await dbContext.Clients.FindAsync(clientToDelete.ClientId));
-        }
-
-        [Fact]
-        public async Task GetClient_ReturnsOk_WhenClientExists()
-        {
-            // Arrange
-            var dbContext = GetDatabaseContext();
-            var existingClient = new Client { FirstName = "Test", LastName = "Id", CorporateName = "T", CUIT = "20-11122233-4", Email = "id@test.com", CellPhone = "1111111111", Birthdate = DateTime.Now };
-            dbContext.Clients.Add(existingClient);
-            await dbContext.SaveChangesAsync();
-
-            var controller = new ClientsController(dbContext, new Mock<ILogger<ClientsController>>().Object);
-
-            // Act
-            var result = await controller.GetClient(existingClient.ClientId);
-
-            // Assert
-            var actionResult = Assert.IsType<OkObjectResult>(result.Result);
-            var returnedClient = Assert.IsType<Client>(actionResult.Value);
-            Assert.Equal(existingClient.ClientId, returnedClient.ClientId);
-        }
-
-        [Fact]
-        public async Task GetClient_ReturnsNotFound_WhenIdDoesNotExist()
-        {
-            // Arrange
-            var dbContext = GetDatabaseContext();
-            var controller = new ClientsController(dbContext, new Mock<ILogger<ClientsController>>().Object);
-
-            // Act
-            var result = await controller.GetClient(999);
-
-            // Assert
-            Assert.IsType<NotFoundObjectResult>(result.Result);
-        }
-
-        [Fact]
-        public async Task CreateClient_ReturnsConflict_WhenCuitExists()
-        {
-            // Arrange
-            var dbContext = GetDatabaseContext();
-
-            dbContext.Clients.Add(new Client { FirstName = "A", LastName = "B", CorporateName = "C", CUIT = "20-12345678-9", Email = "a@test.com", CellPhone = "1111111111", Birthdate = DateTime.Now });
-            await dbContext.SaveChangesAsync();
-
-            var controller = new ClientsController(dbContext, new Mock<ILogger<ClientsController>>().Object);
-
-            var dtoConCuitDuplicado = new CreateClientDto
+            var newDto = new CreateClientDto
             {
                 FirstName = "X",
                 LastName = "Y",
                 CorporateName = "Z",
-                CUIT = "20-12345678-9",
-                Email = "otro@test.com",
-                CellPhone = "2222222222",
+                CUIT = "20-11111111-1",
+                Email = "duplicado@test.com",
+                CellPhone = "111",
                 Birthdate = DateTime.Now
             };
 
+            var existingClient = new Client
+            {
+                ClientId = 1,
+                FirstName = "A",
+                LastName = "B",
+                CorporateName = "C",
+                CUIT = "20-99999999-9",
+                Email = "duplicado@test.com",
+                CellPhone = "222",
+                Birthdate = DateTime.Now
+            };
+
+            _mockRepo.Setup(repo => repo.GetConflictAsync(newDto.CUIT, newDto.Email))
+                     .ReturnsAsync(existingClient);
+
             // Act
-            var result = await controller.CreateClient(dtoConCuitDuplicado);
+            var result = await _controller.CreateClient(newDto);
 
             // Assert
             var conflictResult = Assert.IsType<ConflictObjectResult>(result.Result);
-            Assert.Contains("20-12345678-9", conflictResult.Value?.ToString());
+            Assert.Contains("email", conflictResult.Value?.ToString());
         }
-
         [Fact]
-        public async Task UpdateClient_ReturnsOk_WhenUpdateIsValid()
+        public async Task UpdateClient_ReturnsOk_WhenValid()
         {
             // Arrange
-            var dbContext = GetDatabaseContext();
-            var originalClient = new Client { FirstName = "Viejo", LastName = "Nombre", CorporateName = "Old Corp", CUIT = "20-11111111-1", Email = "old@test.com", CellPhone = "1111111111", Birthdate = DateTime.Now };
-            dbContext.Clients.Add(originalClient);
-            await dbContext.SaveChangesAsync();
+            var existingClient = new Client { ClientId = 10, FirstName = "Old", LastName = "Old", CorporateName = "Old", CUIT = "20-10101010-1", Email = "old@t.com", CellPhone = "111", Birthdate = DateTime.Now };
 
-            var controller = new ClientsController(dbContext, new Mock<ILogger<ClientsController>>().Object);
+            _mockRepo.Setup(repo => repo.GetByIdAsync(10))
+                     .ReturnsAsync(existingClient);
 
-            originalClient.FirstName = "Nuevo Nombre";
-            originalClient.CorporateName = "New Corp";
+            _mockRepo.Setup(repo => repo.EmailExistsForOtherClientAsync("new@t.com", 10))
+                     .ReturnsAsync(false);
+
+            var updateData = new Client { ClientId = 10, FirstName = "New", LastName = "New", CorporateName = "New", CUIT = "20-10101010-1", Email = "new@t.com", CellPhone = "111", Birthdate = DateTime.Now };
 
             // Act
-            var result = await controller.UpdateClient(originalClient.ClientId, originalClient);
+            var result = await _controller.UpdateClient(10, updateData);
 
             // Assert
-            var actionResult = Assert.IsType<OkObjectResult>(result.Result);
-            var updatedClient = Assert.IsType<Client>(actionResult.Value);
-            Assert.Equal("Nuevo Nombre", updatedClient.FirstName);
-
-            var dbClient = await dbContext.Clients.FindAsync(originalClient.ClientId);
-            
-            Assert.NotNull(dbClient);
-            Assert.Equal("New Corp", dbClient.CorporateName);
+            Assert.IsType<OkObjectResult>(result.Result);
+            _mockRepo.Verify(repo => repo.UpdateAsync(It.IsAny<Client>()), Times.Once);
         }
 
         [Fact]
         public async Task UpdateClient_ReturnsBadRequest_WhenIdsDoNotMatch()
         {
             // Arrange
-            var dbContext = GetDatabaseContext();
-            var controller = new ClientsController(dbContext, new Mock<ILogger<ClientsController>>().Object);
-
-            var clientData = new Client { ClientId = 1, FirstName = "Test", LastName = "Test", CorporateName = "T", CUIT = "20-11111111-1", Email = "t@t.com", CellPhone = "111", Birthdate = DateTime.Now };
+            var updateData = new Client { ClientId = 1, FirstName = "T", LastName = "T", CorporateName = "T", CUIT = "20-111", Email = "t@t.com", CellPhone = "111", Birthdate = DateTime.Now };
 
             // Act
-            var result = await controller.UpdateClient(50, clientData);
+            var result = await _controller.UpdateClient(50, updateData);
 
             // Assert
             Assert.IsType<BadRequestObjectResult>(result.Result);
+            _mockRepo.Verify(repo => repo.GetByIdAsync(It.IsAny<int>()), Times.Never);
         }
 
         [Fact]
         public async Task UpdateClient_ReturnsNotFound_WhenClientDoesNotExist()
         {
             // Arrange
-            var dbContext = GetDatabaseContext();
-            var controller = new ClientsController(dbContext, new Mock<ILogger<ClientsController>>().Object);
+            var updateData = new Client { ClientId = 99, FirstName = "T", LastName = "T", CorporateName = "T", CUIT = "20-111", Email = "t@t.com", CellPhone = "111", Birthdate = DateTime.Now };
 
-            var clientData = new Client { ClientId = 99, FirstName = "Test", LastName = "Test", CorporateName = "T", CUIT = "20-11111111-1", Email = "t@t.com", CellPhone = "111", Birthdate = DateTime.Now };
+            _mockRepo.Setup(repo => repo.GetByIdAsync(99))
+                     .ReturnsAsync((Client?)null);
 
             // Act
-            var result = await controller.UpdateClient(99, clientData);
+            var result = await _controller.UpdateClient(99, updateData);
 
             // Assert
             Assert.IsType<NotFoundObjectResult>(result.Result);
+            _mockRepo.Verify(repo => repo.UpdateAsync(It.IsAny<Client>()), Times.Never);
         }
 
         [Fact]
         public async Task DeleteClient_ReturnsNotFound_WhenClientDoesNotExist()
         {
             // Arrange
-            var dbContext = GetDatabaseContext();
-            var controller = new ClientsController(dbContext, new Mock<ILogger<ClientsController>>().Object);
+            _mockRepo.Setup(repo => repo.GetByIdAsync(999))
+                     .ReturnsAsync((Client?)null);
 
             // Act
-            var result = await controller.DeleteClient(999);
+            var result = await _controller.DeleteClient(999);
 
             // Assert
             Assert.IsType<NotFoundObjectResult>(result);
+            _mockRepo.Verify(repo => repo.DeleteAsync(It.IsAny<Client>()), Times.Never);
+        }
+
+        [Fact]
+        public async Task DeleteClient_ReturnsNoContent_WhenClientExists()
+        {
+            // Arrange
+            var clientToDelete = new Client { ClientId = 1, FirstName = "Bye", LastName = "Bye", CorporateName = "B", CUIT = "20-11111111-1", Email = "b@t.com", CellPhone = "111", Birthdate = DateTime.Now };
+
+            _mockRepo.Setup(repo => repo.GetByIdAsync(1))
+                     .ReturnsAsync(clientToDelete);
+
+            // Act
+            var result = await _controller.DeleteClient(1);
+
+            // Assert
+            Assert.IsType<NoContentResult>(result);
+            _mockRepo.Verify(repo => repo.DeleteAsync(clientToDelete), Times.Once);
         }
     }
 }
