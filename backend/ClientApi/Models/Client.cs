ab2c8f67447309f022d7c9b8518939d8a78c9757
frighste <frighettostefano@gmail.com>
2025-11-21 11:55:48 -0300

    Make ClientId Json Required

--- backend/ClientApi/Models/Client.cs
+++ backend/ClientApi/Models/Client.cs
@@ -1,10 +1,12 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 
 namespace ClientApi.Models
 {
     public class Client
     {
-        public int ClientId { get; set; }
+        [JsonRequired]
+        public int? ClientId { get; set; }
 
         [Required]
         public required string FirstName { get; set; }
